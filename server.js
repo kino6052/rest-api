793a9bbeab0084320296e0cb9a88ca27b37884bb
Kirill Novik <jimmynovik@gmail.com>
2016-05-24 22:49:35 +0000

    Middleware Protecting Routes

--- server.js
+++ server.js
@@ -28,6 +28,25 @@ apiRoutes.get('/', function(req, res){
    });
 });
 
+apiRoutes.use(function (req, res, next){
+    var token = req.body.token || req.query.token || req.headers['x-access-token'];
+    if (token) {
+        jwt.verify(token, app.get('superSecret'), function(err, decoded){
+            if (err) {
+                return res.json({success: false, message: 'Failed to Authenticate Token.'});
+            } else {
+                req.decoded = decoded;
+                next();
+            }
+        });
+    } else {
+        return res.status(403).send({
+            success: false,
+            message: 'No Token Provided.'
+        });
+    }
+});
+
 apiRoutes.get('/users', function(req, res){
    User.find({}, function(err, users){
        res.json(users);
